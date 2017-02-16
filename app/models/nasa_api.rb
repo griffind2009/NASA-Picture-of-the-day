8ea6aaa162e8b46681578b6d5c700a5db6b15573
Domonique Griffin <griffind2009@gmail.com>
2017-02-15 19:33:24 -0500

    add logic to model

--- app/models/nasa_api.rb
+++ app/models/nasa_api.rb
@@ -1,2 +1,7 @@
 class NasaApi < ApplicationRecord
+  def self.get_api_info
+    nasa_api_url = "https://api.nasa.gov/planetary/apod?api_key=#{ENV['NASA_API_KEY']}"
+    request_to_nasa = Net::HTTP.get(URI(nasa_api_url))
+    JSON.parse(request_to_nasa)
+  end
 end
